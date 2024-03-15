66eacb6cf8c1a6b423547295adac87c20e2fac5d
ThomasHWik <146004751+ThomasHWik@users.noreply.github.com>
2024-03-15 14:28:48 +0100

    feat: added use case 6 sql query

--- src/DB2_use_case_queries.sql
+++ src/DB2_use_case_queries.sql
@@ -39,3 +39,19 @@ NATURAL INNER JOIN Rolle
 INNER JOIN Skuespiller ON (OppsetningAvSkuespill.SkuespillerID = Skuespiller.PersonID)
 INNER JOIN Person USING (PersonID);
 
+
+-- Use case 6
+
+/*
+	Vi ønsker å lage et query i SQL som finner hvilke forestillinger som har solgt 
+	best. Skriv ut navn på forestilling og dato og antall solgte plasser sortert på 
+	antall plasser i synkende rekkefølge.
+*/
+
+SELECT Skuespill.Tittel AS 'Teaterstykke', Forestilling.Dato, COUNT(Billett.BillettID) AS AntallBilletterSolgt
+FROM Forestilling
+LEFT OUTER JOIN ForestillingBillett USING (ForestillingID)
+LEFT OUTER JOIN Billett USING (BillettID)
+INNER JOIN Skuespill USING (Skuespill_ID)
+GROUP BY Forestilling.ForestillingID
+ORDER BY AntallBilletterSolgt DESC;
